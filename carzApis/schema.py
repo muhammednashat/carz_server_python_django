604ca4a63b1f4ebf718d7bfe852685968bef5602
muhammednashat <mnashat.dev@gmail.com>
2025-08-12 11:24:23 +0300

    create api for login and test it

--- carzApis/schema.py
+++ carzApis/schema.py
@@ -11,6 +11,7 @@ class UserType(DjangoObjectType):
         fields = ("id", "name", "email")
 
 
+
 class GetUserByIdMutation(graphene.Mutation):
     class Arguments:
         id = graphene.ID()
@@ -52,8 +53,32 @@ class UpdateUserMutation(graphene.Mutation):
        user.name = name
        user.save()
        return cls(user = user)
+class SignInMutation(graphene.Mutation):
+    class Arguments:
+        email = graphene.String()
+        lang = graphene.String()    
+        
+        
+    status = graphene.Boolean()
+    msg = graphene.String()
+    user = graphene.Field(UserType)
     
-
+    @classmethod
+    def mutate(cls,root, info, email, lang):
+        try:
+           user = UserModle.objects.all().get(email = email)
+           if(user):
+              print("of")  
+              return cls(status = True , msg = "ok", user = user)
+           else:
+              print("oe")  
+              return cls(status = True , msg = "ok")
+                   
+        except Exception as e:
+           print(e)  
+           return cls(status = False, msg = "null")
+       
+        
 
 class CreatUserMutation(graphene.Mutation):
     class Arguments:
@@ -73,11 +98,18 @@ class Mutation(graphene.ObjectType ):
    create_user = CreatUserMutation.Field()             
    delete_user = DeletUserMutatoion.Field()
    getUserById = GetUserByIdMutation.Field()
-   
+   signIn= SignInMutation.Field()
     
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
-
+    sign_in = graphene.Field(UserType, email = graphene.String())
+    
+    def resolve_sign_in(root, inf, email):
+        print(f"signIn => {email}")
+        user = UserModle.objects.all().get(email = email)
+        print(f"user {user}")
+        return user
+        
     def resolve_users(root, info):
         return UserModle.objects.all()
 
